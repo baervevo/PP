bf1ef41f26b8356be10fc6e1406059ddd9e3ecaf
baerVEVO <mariuszrychlicki04@gmail.com>
2024-12-08 23:04:25 +0100

    Optimized pathHelper

--- LAB/Lab_4/Z2.ml
+++ LAB/Lab_4/Z2.ml
@@ -9,18 +9,23 @@ type disk = {
 };;
 
 let path disk asset =
-  let rec pathHelper queue =
-    match queue with
-    | [] -> None
+  let rec pathHelper front back =
+    match front with
+    | [] ->
+        if back = [] then None
+        else pathHelper (List.rev back) []
     | (File {name}, curr_path) :: tl ->
         if name = asset then Some (name :: curr_path)
-        else pathHelper tl
+        else pathHelper tl back
     | (Folder {name; contents}, curr_path) :: tl ->
         if name = asset then Some (name :: curr_path)
-        else pathHelper (tl @ (List.map (fun x -> (x, name :: curr_path)) contents))
+        else
+          let contents_mapped_to_path = List.map (fun x -> (x, name :: curr_path)) contents in
+          let new_back = List.rev_append contents_mapped_to_path back in
+          pathHelper tl new_back
   in
 
-  match pathHelper (List.map (fun x -> (x, [])) disk.contents) with
+  match pathHelper (List.map (fun x -> (x, [])) disk.contents) [] with
   | None -> None
   | Some path -> Some ((String.make 1 disk.label) ^ ":" ^ (List.fold_right (fun acc x -> x ^ "/" ^ acc) path ""));;
 
